0c3db551e1e2eb589c3c5b62ff2fd0d27540cb8c
Juan Martinez <martinezalonsojn@gmail.com>
2023-09-25 14:28:39 +0200

    Add BLCU Ack pipeline

---
+++ pkg/pipeline/pipes/blcu_ack/blcu_ack.go
@@ -0,0 +1,27 @@
+package blcuAck
+
+import (
+	"io"
+
+	"github.com/HyperloopUPV-H8/h9-backend/pkg/pipeline"
+)
+
+type Packet struct {
+	id pipeline.PacketId
+}
+
+func (packet Packet) Id() pipeline.PacketId {
+	return packet.id
+}
+
+type Pipe struct {
+	output chan<- pipeline.Packet
+}
+
+func (pipe *Pipe) SetOutput(output chan<- pipeline.Packet) { pipe.output = output }
+
+func (pipe Pipe) ReadPacket(id pipeline.PacketId, reader io.Reader) (int, error) {
+	pipe.output <- Packet{id}
+
+	return 0, nil
+}
