72718b52d40d7811d6022f905b7ae6b946148d92
Juan Martinez <martinezalonsojn@gmail.com>
2023-09-29 18:08:52 +0200

    Add data constructor

--- pkg/pipeline/pipes/data/constructor.go
+++ pkg/pipeline/pipes/data/constructor.go
@@ -1,7 +1,21 @@
 package data
 
-func NewPipe() Pipe {
+import (
+	"encoding/binary"
+
+	"github.com/HyperloopUPV-H8/h9-backend/pkg/pipeline"
+)
+
+func NewPipe(codec *Codec) Pipe {
 	return Pipe{
-		// TODO
+		codec: codec,
+	}
+}
+
+func NewCodec(structures map[pipeline.PacketId]PacketStructure, enums map[ValueName]EnumDescriptor, byteOrder binary.ByteOrder) Codec {
+	return Codec{
+		packetStructures: structures,
+		enums:            enums,
+		byteOrder:        byteOrder,
 	}
 }
